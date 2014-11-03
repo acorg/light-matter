a04f852cbe868f5285a3d4b1b36c5c7481adc8fd
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2014-11-03 16:14:10 +0000

    changed comments

--- light/statistics/bunyaviridae.py
+++ light/statistics/bunyaviridae.py
@@ -1,25 +1,34 @@
+import re
+
 from light.statistics import Statistic
 
 ORTHOBUNYA_3 = 'TCATCACATGA'
 ORTHOBUNYA_5 = 'TCGTGTGATGA'
 HANTA_3 = 'ATCATCATCTG'
 HANTA_5 = 'ATGATGAT'
-NAIRO_3 = 'AGAGTCT'
+NAIRO_3 = 'AGAGTTTCT'
 NAIRO_5 = 'AGAAACTCT'
-PHLEBO_3 = 'TGTGTC'
+PHLEBO_3 = 'TGTGTTTC'
 PHLEBO_5 = 'GAAACACA'
-TOSPO_3 = 'TCTCGTAG'
+TOSPO_3 = 'TCTCGTTAG'
 TOSPO_5 = 'CTAACGAGA'
 ORTHOBUNYA_3_RC = 'TCATGTGATGA'
 ORTHOBUNYA_5_RC = 'TCATCACACGA'
 HANTA_3_RC = 'CAGATGATGAT'
 HANTA_5_RC = 'ATCATCAT'
-NAIRO_3_RC = 'AGACTCT'
+NAIRO_3_RC = 'AGAAACTCT'
 NAIRO_5_RC = 'AGAGTTTCT'
-PHLEBO_3_RC = 'GACACA'
+PHLEBO_3_RC = 'GAAACACA'
 PHLEBO_5_RC = 'TGTGTTTC'
-TOSPO_3_RC = 'CTACGAGA'
+TOSPO_3_RC = 'CTAACGAGA'
 TOSPO_5_RC = 'TCTCGTTAG'
+
+BUNYAVIRIDAES = re.compile('|'.join([ORTHOBUNYA_3, ORTHOBUNYA_5,
+                                    ORTHOBUNYA_3_RC, ORTHOBUNYA_5_RC, HANTA_3,
+                                    HANTA_5, HANTA_3_RC, HANTA_5_RC, NAIRO_3,
+                                    NAIRO_5, NAIRO_3_RC, NAIRO_5_RC, PHLEBO_3,
+                                    PHLEBO_5, PHLEBO_3_RC, PHLEBO_5_RC,
+                                    TOSPO_3, TOSPO_5, TOSPO_3_RC, TOSPO_5_RC]))
 
 
 class Bunyaviridae(Statistic):
@@ -29,7 +38,7 @@ class Bunyaviridae(Statistic):
     """
     NAME = 'Bunyaviridae'
     SUPPORTED_TYPES = 'dna'
-    MIN_LENGTH = 11
+    MIN_LENGTH = 8
 
     def findBunyaviridae(sequence):
         """
@@ -37,15 +46,7 @@ class Bunyaviridae(Statistic):
 
         @param sequence: a nucleotide sequence.
         """
-        bunyaviridaes = [ORTHOBUNYA_3, ORTHOBUNYA_5, ORTHOBUNYA_3_RC,
-                         ORTHOBUNYA_5_RC, HANTA_3, HANTA_5, HANTA_3_RC,
-                         HANTA_5_RC, NAIRO_3, NAIRO_5, NAIRO_3_RC, NAIRO_5_RC,
-                         PHLEBO_3, PHLEBO_5, PHLEBO_3_RC, PHLEBO_5_RC, TOSPO_3,
-                         TOSPO_5, TOSPO_3_RC, TOSPO_5_RC]
-
-        for virus in bunyaviridaes:
-            if virus in sequence:
-                return True
+        return BUNYAVIRIDAES.search(sequence)
 
     def _evaluate(self, read):
         """
@@ -55,5 +56,4 @@ class Bunyaviridae(Statistic):
         @return: C{bool} True if any of the bunyavirus sequences match.
         """
         bunyavirus = self.findBunyaviridae(read.sequence)
-        if bunyavirus:
-            return True
+        return bunyavirus

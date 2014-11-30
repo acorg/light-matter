90062e9740e0cf7541ad5eb7dcb3416f63cef6c8
terrycojones <terry@fluidinfo.com>
2014-11-30 11:41:41 +0000

    Fixed finder name listing and another small bug.

--- light/trig/__init__.py
+++ light/trig/__init__.py
@@ -6,9 +6,9 @@ ALL_TRIG_FINDER_CLASSES = {Peaks, Troughs}
 
 def find(name):
     """
-    A function to find a landmark finder.
+    A function to find a trig point finder.
 
-    @param name: The name of the landmark finder to find.
+    @param name: The name of the trig point finder to find.
     """
 
     for klass in ALL_TRIG_FINDER_CLASSES:
@@ -16,5 +16,5 @@ def find(name):
             return klass
 
 
-# Default exports for 'from light.landmarks import *'
+# Default exports for 'from light.trig import *'
 __all__ = ['find', 'ALL_TRIG_FINDER_CLASSES']

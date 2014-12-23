eccface161716070a98dc0f791554b4c571f98af
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2014-12-23 22:48:40 +0100

    fixed failing test

--- test/test_alignments.py
+++ test/test_alignments.py
@@ -255,8 +255,8 @@ class TestLightReadsAlignments(TestCase):
         with patch.object(bz2, 'BZ2File') as mockMethod:
             mockMethod.side_effect = sideEffect.sideEffect
             error = 'Database and output file have different checksums.'
-            readsAlignments = LightReadsAlignments(['file.json.bz2'], DB)
-            self.assertRaisesRegexp(ValueError, error, list, readsAlignments)
+            self.assertRaisesRegexp(ValueError, error, LightReadsAlignments,
+                                    ['file.json.bz2'], DB)
 
     def testGetSubjectSequence(self):
         """

6adeef6e216acf43d564e1585e654c3af1200f92
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2015-01-07 17:31:27 +0000

    added ipynb.py script for importing

---
+++ light/ipynb.py
@@ -0,0 +1,23 @@
+# Import and make available things that are convenient to have around in
+# iPythonNotebook following 'from light.ipynb import *'.
+
+from dark.fasta import FastaReads
+from dark.titles import titleCounts, TitlesAlignments
+from dark.html import (
+    summarizeTitlesByLength, summarizeTitlesByMaxScore,
+    summarizeTitlesByMedianScore, summarizeTitlesByCount,
+    summarizeTitlesByTitle)
+from .alignments import LightReadsAlignments
+from .graphics import plotFeatures
+
+# Keep pyflakes quiet by pretending to make use of all our imports.
+_ = (FastaReads, LightReadsAlignments, titleCounts, TitlesAlignments,
+     summarizeTitlesByLength, summarizeTitlesByMaxScore,
+     summarizeTitlesByMedianScore, summarizeTitlesByCount,
+     summarizeTitlesByTitle, plotFeatures)
+
+__all__ = [
+    'FastaReads', 'LightReadsAlignments', 'titleCounts', 'TitlesAlignments',
+    'summarizeTitlesByLength', 'summarizeTitlesByMaxScore',
+    'summarizeTitlesByMedianScore', 'summarizeTitlesByCount',
+    'summarizeTitlesByTitle', 'plotFeatures']

3f49a718f6d7901b1d247ed12782342fc4f20b85
terrycojones <terry@fluidinfo.com>
2015-01-09 17:30:01 +0000

    Made the database performance testing more useful. Now tests all single landmark and trig point finders via multiple calls to addSubject.

--- performance/perf_database.py
+++ performance/perf_database.py
@@ -4,23 +4,29 @@ from unittest import TestCase
 from dark.reads import AARead
 
 from light.database import Database
-from light.landmarks.alpha_helix import AlphaHelix
-from light.landmarks.beta_strand import BetaStrand
+from light.landmarks import (
+    AlphaHelix, AlphaHelix_3_10, AlphaHelix_pi, BetaStrand, BetaTurn)
+from light.trig import (
+    AminoAcids, Peaks, Troughs, IndividualPeaks, IndividualTroughs)
 
 
-class TestDatabase(TestCase):
+class TestDatabaseCreation(TestCase):
     """
-    Test database build performance.
+    Test database creation performance.
     """
 
     def testCreation(self):
         """
-        How long does it take to construct a database with just one subject if
-        there are no landmark or trig point finders?
+        How long does it take to construct a database with no subjects, and no
+        landmark or trig point finders?
         """
-        database = Database([], [])
-        read = AARead('id', 'MTMTSTTSNLPGILSQPSSELLTNWYAEQVVQGHIL')
-        database.addSubject(read)
+        Database([], [])
+
+
+class TestDatabaseChecksum(TestCase):
+    """
+    Test database checksum performance.
+    """
 
     def testChecksumEmpty(self):
         """
@@ -47,34 +53,148 @@ class TestDatabase(TestCase):
         self.details = elapsed
 
 
-class _TestSubjectAddMixin(object):
+class _SubjectAddMixin(object):
     """
     Test database subject adding performance.
     """
 
-    def testAdd1000Subjects(self):
+    # The number of copies of a known feature that should be present in the
+    # sequence in the read that is added to the database. See our subclasses
+    # for usage.
+    FEATURE_COUNT = 50
+
+    # The number of subjects to add to the database.
+    SUBJECT_COUNT = 50
+
+    def testAddSubjects(self):
         """
-        How long does it take to add 1000 subjects to a database.
+        How long does it take to add SUBJECT_COUNT subjects to a database.
         """
-        read = AARead('id', 'MTMTSTTSNLPGILSQPSSELLTNWYAEQVVQGHIL')
+        read = AARead('id', self.FEATURE_SEQUENCE * self.FEATURE_COUNT)
         database = Database(self.LANDMARKS, self.TRIG_POINTS)
         startTime = time.time()
-        for _ in xrange(1000):
+        for _ in xrange(self.SUBJECT_COUNT):
             database.addSubject(read)
         elapsed = time.time() - startTime
         self.details = elapsed
 
 
-class SubjectAddNoLandmarksNoTrigPoints(_TestSubjectAddMixin, TestCase):
+class SubjectAddNoFinders(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has no landmark or
+    trig point finders.
+    """
     LANDMARKS = []
     TRIG_POINTS = []
+    # The sequence below be random. It's not going to be examined by
+    # anything because there are no landmark or trig point finders.
+    FEATURE_SEQUENCE = 'MMMMMMMMMM'
 
 
-class SubjectAddBetaStrand(_TestSubjectAddMixin, TestCase):
+class SubjectAddBetaStrand(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has a BetaStrand
+    landmark finder.
+    """
     LANDMARKS = [BetaStrand]
     TRIG_POINTS = []
+    SEP = 'N'
+    FEATURE_SEQUENCE = BetaStrand.BETA_STRAND_AAs + SEP
+
+    def testSeparatorIsntABetaStrandResidue(self):
+        """
+        The residue we use to separate Beta strands in the test sequence
+        must not itself be a Beta strand residue.
+        """
+        self.assertNotIn(self.SEP, BetaStrand.BETA_STRAND_AAs)
+        self.ignore = True
 
 
-class SubjectAddAlphaHelix(_TestSubjectAddMixin, TestCase):
+class SubjectAddBetaTurn(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has a BetaTurn
+    landmark finder.
+    """
+    LANDMARKS = [BetaTurn]
+    TRIG_POINTS = []
+    FEATURE_SEQUENCE = 'NPNWA'
+
+
+class SubjectAddAlphaHelix(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an AlphaHelix
+    landmark finder.
+    """
     LANDMARKS = [AlphaHelix]
     TRIG_POINTS = []
+    FEATURE_SEQUENCE = 'FRRRFRRRFA'
+
+
+class SubjectAddAlphaHelix_3_10(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an
+    AlphaHelix_3_10 landmark finder.
+    """
+    LANDMARKS = [AlphaHelix_3_10]
+    TRIG_POINTS = []
+    FEATURE_SEQUENCE = 'FRRFRRFA'
+
+
+class SubjectAddAlphaHelix_pi(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an
+    AlphaHelix_pi landmark finder.
+    """
+    LANDMARKS = [AlphaHelix_pi]
+    TRIG_POINTS = []
+    FEATURE_SEQUENCE = 'FRRRRFRRRRFA'
+
+
+class SubjectAddAminoAcids(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an
+    AminoAcids trig point finder.
+    """
+    LANDMARKS = []
+    TRIG_POINTS = [AminoAcids]
+    FEATURE_SEQUENCE = 'CMMMMMMM'
+
+
+class SubjectAddIndividualPeaks(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an
+    IndividualPeaks trig point finder.
+    """
+    LANDMARKS = []
+    TRIG_POINTS = [IndividualPeaks]
+    FEATURE_SEQUENCE = 'ATAAAA'
+
+
+class SubjectAddIndividualTroughs(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an
+    IndividualTroughs trig point finder.
+    """
+    LANDMARKS = []
+    TRIG_POINTS = [IndividualTroughs]
+    FEATURE_SEQUENCE = 'TMTTTT'
+
+
+class SubjectAddPeaks(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an
+    Peaks trig point finder.
+    """
+    LANDMARKS = []
+    TRIG_POINTS = [Peaks]
+    FEATURE_SEQUENCE = 'ASAAAA'
+
+
+class SubjectAddTroughs(_SubjectAddMixin, TestCase):
+    """
+    A class to test adding subjects to a database that just has an
+    Troughs trig point finder.
+    """
+    LANDMARKS = []
+    TRIG_POINTS = [Troughs]
+    FEATURE_SEQUENCE = 'AVAAAA'

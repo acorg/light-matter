faff9a362afa76ea0dfcb3453f43a69aeab92f9d
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2015-01-12 14:52:52 +0000

    added bucketFactor to bin scripts

--- light/performance/query.py
+++ light/performance/query.py
@@ -7,7 +7,7 @@ from light.database import Database
 
 def queryDatabase(databaseFile, queryFile, landmarkFinders, trigFinders,
                   limitPerLandmark=None, maxDistance=None, minDistance=None,
-                  aboveMeanThreshold=None):
+                  aboveMeanThreshold=None, bucketFactor=1):
     """
     Build a database, query it, return results.
 
@@ -21,12 +21,14 @@ def queryDatabase(databaseFile, queryFile, landmarkFinders, trigFinders,
         landmark per read.
     @param maxDistance: The maximum distance permitted between yielded pairs.
     @param minDistance: The minimum distance permitted between yielded pairs.
+    @param bucketFactor: A C{int} factor by which the distance between
+        landmark and trig point is divided, to influence sensitivity.
     @return: A C{dict} whose keys are query ids and whose values are C{dict}s
         that map subject ids to scores. I.e., for each read we provide a
         C{dict} showing what subjects it matched, and with what score.
     """
     database = Database(landmarkFinders, trigFinders, limitPerLandmark,
-                        maxDistance, minDistance)
+                        maxDistance, minDistance, bucketFactor)
     if isinstance(queryFile, Reads):
         queries = queryFile
     else:

772128e4435118f7588673df2b9c515d10277463
terrycojones <terry@fluidinfo.com>
2015-03-12 11:38:58 +0100

    TRIVIAL: added significanceFraction to arguments to affinityMatrix

--- light/performance/affinity.py
+++ light/performance/affinity.py
@@ -4,13 +4,17 @@ from dark.fasta import FastaReads
 from light.database import DatabaseSpecifier
 
 
-def affinityMatrix(sequences, **kwargs):
+def affinityMatrix(sequences, significanceFraction=None, **kwargs):
     """
     Produce an affinity matrix containing scores for a set of reads matched
     against the subjects in a database.
 
     @param sequences: Either A C{str} filename of sequences to consider or
         a C{light.reads.Reads} instance.
+    @param significanceFraction: The C{float} fraction of all (landmark,
+        trig point) pairs for a scannedRead that need to fall into the
+        same histogram bucket for that bucket to be considered a
+        significant match with a database title.
     @param kwargs: See
         C{database.DatabaseSpecifier.getDatabaseFromKeywords} for
         additional keywords, all of which are optional.
@@ -29,7 +33,8 @@ def affinityMatrix(sequences, **kwargs):
     for readIndex, read in enumerate(reads):
         row = []
         append = row.append
-        analysis = db.find(read).analysis
+        analysis = db.find(
+            read, significanceFraction=significanceFraction).analysis
         for subjectIndex in xrange(subjectCount):
             try:
                 score = analysis[subjectIndex]['score']

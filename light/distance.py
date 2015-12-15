7ff9533fadb0783e02c3bb2b65cdca7d1022fc5d
terrycojones <terry@jon.es>
2015-12-15 23:06:07 +0000

    Make sure we test both the Python and the C version of the scale function.

--- light/distance.py
+++ light/distance.py
@@ -1,46 +1,59 @@
 from __future__ import division
 
+from math import log
+from warnings import warn
+
+
+def _pp_scale(dist, base):
+    """
+    Use a log scale to adjust a distance.
+
+    By logging distances (usually with a base like 1.1) we can map a range
+    of distances onto a smaller range. So some different input distances
+    will have the same scaled valued. E.g., with base 1.1, the distance
+    range 0-250 is mapped onto the range 0-57.
+
+    The mapping is not linear. Many larger distances will be mapped to the
+    same scaled distance. E.g., with base 1.1, 200-207 all are mapped to
+    55, 208-228 are all mapped to 56, and 229-250 are all mapped to 57. So
+    at the upper end of the distance range 0-250 the 1.1 logarithm base
+    allows for about 10% sloppiness in adjusted distances. E.g., distance
+    230 is adjusted to the same value (57) as distance 245.  On the other
+    hand, values 0-12 are all mapped to distinct values, which means
+    distances are strictly interpreted among those small values.
+
+    @param dist: An C{int} distance, usually a distance between two
+        features in an AA sequence.
+    @param base: The C{float} logarithmic base to use in scaling.
+    @raise ValueError: If C{base} is zero.
+    @return: An C{int} scaled distance.
+    """
+    if dist > 0:
+        result = int(log(dist) / log(base)) if base != 1.0 else dist
+        return min([result, dist])
+    elif dist < 0:
+        result = -1 * int(log(-dist) / log(base)) if base != 1.0 else dist
+        return max([result, dist])
+    else:
+        return 0
+
 try:
     from light._distance import lib
 except ImportError as e:
-    from warnings import warn
     warn('Could not import cffi scale function (%s). Using pure Python '
          'version.' % e)
-
-    from math import log
-
-    def scale(dist, base):
-        """
-        Use a log scale to adjust a distance.
-
-        By logging distances (usually with a base like 1.1) we can map a range
-        of distances onto a smaller range. So some different input distances
-        will have the same scaled valued. E.g., with base 1.1, the distance
-        range 0-250 is mapped onto the range 0-57.
-
-        The mapping is not linear. Many larger distances will be mapped to the
-        same scaled distance. E.g., with base 1.1, 200-207 all are mapped to
-        55, 208-228 are all mapped to 56, and 229-250 are all mapped to 57. So
-        at the upper end of the distance range 0-250 the 1.1 logarithm base
-        allows for about 10% sloppiness in adjusted distances. E.g., distance
-        230 is adjusted to the same value (57) as distance 245.  On the other
-        hand, values 0-12 are all mapped to distinct values, which means
-        distances are strictly interpreted among those small values.
-
-        @param dist: An C{int} distance, usually a distance between two
-            features in an AA sequence.
-        @param base: The C{float} logarithmic base to use in scaling.
-        @raise ValueError: If C{base} is zero.
-        @return: An C{int} scaled distance.
-        """
-        if dist > 0:
-            result = int(log(dist) / log(base)) if base != 1.0 else dist
-            return min([result, dist])
-        elif dist < 0:
-            result = -1 * int(log(-dist) / log(base)) if base != 1.0 else dist
-            return max([result, dist])
-        else:
-            return 0
+    # Use the pure Python version.
+    scale = _pp_scale
 else:
     # Use the C version (see ../src/distance.py).
+    #
+    # Note that this function will return 0.0 if base is 0.0. This is
+    # undesirable but fixing it would require writing a Python function to
+    # test for it happening and raising ValueError if so. That partly
+    # defeats the point of using a C extension. Seeing as it's unlikely
+    # that we'll ever use a distance base of zero (and not get tripped up
+    # by the test for that in parameters.py), and that the results of doing
+    # so would be so spectacularly weird (all distances would be scaled to
+    # zero), I thought it better to just call the C function directly. See
+    # also the tests in ../test/test_distance.py
     scale = lib.scale
--- test/test_distance.py
+++ test/test_distance.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
+from functools import wraps
 
-from light.distance import scale
+from light.distance import scale as defaultScale, _pp_scale
 
 
-class TestDistance(TestCase):
+class TestDistanceMixin(object):
     """
     Tests for the light.distance.scale function.
     """
@@ -12,22 +13,7 @@ class TestDistance(TestCase):
         """
         If the distance is 0, the scale function must return 0.
         """
-        self.assertEqual(0, scale(0, 2.0))
-
-    def testBaseZero(self):
-        """
-        If the base is 0.0, the scale function must raise ValueError using the
-        pure Python version of scale, and return 0.0 if using the C version.
-
-        We shouldn't ever be using a base of zero, and we'll know if we do
-        (when using the C scale function) because all distances will be scaled
-        to zero. Also, we check for distance base <= 0 in parameters.py and
-        raise a C{ValueError} there.
-        """
-        if scale.__doc__ == 'direct call to the C function of the same name':
-            self.assertEqual(0.0, scale(27, 0.0))
-        else:
-            self.assertRaises(ValueError, scale, 27, 0.0)
+        self.assertEqual(0, self.scale(0, 2.0))
 
     def testBaseOne(self):
         """
@@ -35,20 +21,20 @@ class TestDistance(TestCase):
         passed.
         """
         for i in range(0, 100):
-            self.assertEqual(i, scale(i, 1.0))
+            self.assertEqual(i, self.scale(i, 1.0))
 
     def testBaseTwo(self):
         """
         If the base is 2.0, the scale function must return the expected result.
         """
-        self.assertEqual(5, scale(32, 2.0))
+        self.assertEqual(5, self.scale(32, 2.0))
 
     def testNegative(self):
         """
         If the distance is negative, the scale function must return the
         expected result.
         """
-        self.assertEqual(-46, scale(-81, 1.1))
+        self.assertEqual(-46, self.scale(-81, 1.1))
 
     def testNegativeAndPositiveOpposite(self):
         """
@@ -57,8 +43,8 @@ class TestDistance(TestCase):
         must work for small and large values (small means less than 38 with a
         base of 1.1 because int(log base 1.1 38) = 38).
         """
-        self.assertEqual(scale(32, 1.1), -1 * scale(-32, 1.1))
-        self.assertEqual(scale(320, 1.1), -1 * scale(-320, 1.1))
+        self.assertEqual(self.scale(32, 1.1), -1 * self.scale(-32, 1.1))
+        self.assertEqual(self.scale(320, 1.1), -1 * self.scale(-320, 1.1))
 
     def testResultCannotBeLarger(self):
         """
@@ -69,7 +55,7 @@ class TestDistance(TestCase):
         function returns the original value in this case.
         """
         # int(Log base 1.1 of 10) = 24 so we should just have 10 returned.
-        self.assertEqual(10, scale(10, 1.1))
+        self.assertEqual(10, self.scale(10, 1.1))
 
     def testScaleOnePointOneSmallValues(self):
         """
@@ -77,35 +63,91 @@ class TestDistance(TestCase):
         result on small values (for which int(log base 1.1 value) is greater
         than value).
         """
-        self.assertEqual(0, scale(0, 1.1))
-        self.assertEqual(0, scale(1, 1.1))
+        self.assertEqual(0, self.scale(0, 1.1))
+        self.assertEqual(0, self.scale(1, 1.1))
         for i in range(2, 39):
-            self.assertEqual(i, scale(i, 1.1))
+            self.assertEqual(i, self.scale(i, 1.1))
 
     def testOnePointOneRange81to88(self):
         """
         With base is 1.1, distances 81-88 are all mapped to 46.
         """
         for distance in range(81, 89):
-            self.assertEqual(46, scale(distance, 1.1))
+            self.assertEqual(46, self.scale(distance, 1.1))
 
     def testOnePointOneRange200to207(self):
         """
         With base is 1.1, distances 200-207 are all mapped to 55.
         """
         for distance in range(200, 208):
-            self.assertEqual(55, scale(distance, 1.1))
+            self.assertEqual(55, self.scale(distance, 1.1))
 
     def testOnePointOneRange208to228(self):
         """
         With base is 1.1, distances 208-228 are all mapped to 56.
         """
         for distance in range(208, 229):
-            self.assertEqual(56, scale(distance, 1.1))
+            self.assertEqual(56, self.scale(distance, 1.1))
 
     def testOnePointOneRange229to250(self):
         """
         With base is 1.1, distances 229-250 are all mapped to 57.
         """
         for distance in range(229, 251):
-            self.assertEqual(57, scale(distance, 1.1))
+            self.assertEqual(57, self.scale(distance, 1.1))
+
+
+class TestDistanceDefault(TestDistanceMixin, TestCase):
+    """
+    Tests for the light.distance.scale function, which is the
+    C extension version (as verified below).
+    """
+
+    @wraps(defaultScale)
+    def scale(self, dist, base):
+        return defaultScale(dist, base)
+
+    def testIsCExtension(self):
+        """
+        The scale function imported by default must be the C extension.
+        """
+        self.assertEqual(
+            'direct call to the C function of the same name',
+            self.scale.__doc__)
+
+    def testBaseZero(self):
+        """
+        If the base is 0.0, the C scale function returns 0.0.
+
+        We shouldn't ever be using a base of zero, and we'll know if we do
+        (when using the C scale function) because all distances will be scaled
+        to zero. Also, we check for distance base <= 0 in parameters.py and
+        raise a C{ValueError} there.
+        """
+        self.assertEqual(0.0, self.scale(27, 0.0))
+
+
+class TestDistancePurePython(TestDistanceMixin, TestCase):
+    """
+    Tests for the light.distance._pp_scale function, the pure Python
+    version of the scale function.
+    """
+
+    @wraps(_pp_scale)
+    def scale(self, dist, base):
+        return _pp_scale(dist, base)
+
+    def testNotCExtension(self):
+        """
+        The pure Python scale function must not be the C extension.
+        """
+        self.assertNotEqual(
+            'direct call to the C function of the same name',
+            self.scale.__doc__)
+
+    def testBaseZero(self):
+        """
+        If the base is 0.0, the pure Python scale function must raise
+        ValueError.
+        """
+        self.assertRaises(ValueError, self.scale, 27, 0.0)

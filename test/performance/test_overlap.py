cf259ebad5adf9e59278dd6b052646ca76f58443
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2016-01-03 11:58:23 +0000

    Another revision of the overlap code

--- test/performance/test_overlap.py
+++ test/performance/test_overlap.py
@@ -100,33 +100,37 @@ class TestCalculateOverlap(TestCase):
 
         ssAARead = SSAARead('5AMF', sequence, structure)
         seqF, commons, totals = CalculateOverlap.getFeatures(ssAARead)
-
         self.assertEqual({
-            '-': {0, 36, 37, 38, 42, 49, 52, 53, 54, 28, 31},
+            'AlphaHelix': set(),
+            'AlphaHelix_3_10': {21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
+            'AlphaHelix_pi': set(),
+            'AminoAcidsLm': set(),
+            'BetaStrand': set(),
+            'BetaTurn': set(),
             'GOR4AlphaHelix': {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
                                18, 19, 20, 21, 22, 23, 24, 29, 30, 31, 32, 33},
-            'AminoAcidsLm': set(),
-            'E': set(),
+            'GOR4BetaStrand': {53, 54},
             'GOR4Coil': {0, 1, 2, 3, 4, 25, 26, 27, 28, 34, 35, 36, 37, 38, 39,
                          40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52},
-            'Peaks': set(),
-            'AlphaHelix_3_10': {21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
-            'S': {51, 34, 35, 50},
-            'Troughs': set(),
-            'BetaTurn': set(),
             'Prosite': {32, 38, 39, 40, 41, 14, 15, 16, 19, 20, 21, 22, 30,
                         31},
-            'I': set(),
-            'IndividualPeaks': set(),
+            'AminoAcids': set(),
+            'IndividualPeaks': {9, 31},
+            'IndividualTroughs': {49, 10},
+            'Peaks': {3, 35, 5, 38, 7, 40, 9, 12, 44, 15, 47, 18, 50, 22, 27,
+                      31},
+            'Troughs': {1, 33, 4, 37, 6, 39, 8, 10, 42, 13, 46, 49, 21, 53, 24,
+                        30},
+            '-': {0, 36, 37, 38, 42, 49, 52, 53, 54, 28, 31},
+            'E': set(),
+            'G': set(),
             'H': {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
                   18, 19, 20, 21, 22, 23, 24, 25, 26, 45, 46, 47, 48},
-            'T': {32, 33, 39, 40, 41, 43, 44, 27, 29, 30},
-            'G': set(),
-            'AlphaHelix': set(),
-            'AlphaHelix_pi': set(),
-            'IndividualTroughs': set(),
-            'BetaStrand': set(),
-            'AminoAcids': set(),
-            'GOR4BetaStrand': {53, 54}}, seqF)
-        self.assertEqual(342, len(commons))
-        self.assertEqual(342, len(totals))
+            'I': set(),
+            'S': {51, 34, 35, 50},
+            'T': {32, 33, 39, 40, 41, 43, 44, 27, 29, 30}}, seqF)
+        self.assertEqual({49, 10}, commons['IndividualTroughs-Troughs'])
+        self.assertEqual({1, 33, 4, 37, 6, 39, 8, 10, 42, 13, 46, 49, 21, 53,
+                          24, 30}, totals['IndividualTroughs-Troughs'])
+        self.assertEqual(190, len(commons))
+        self.assertEqual(190, len(totals))

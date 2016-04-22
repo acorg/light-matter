0596d2139faf9beef78573fcfb69b121eefd4fc1
terrycojones <terry@jon.es>
2016-04-22 14:58:38 +0100

    Put polymerase performance test output into its own directory and sub-directories.

--- light/performance/test/perf_polymerase.py
+++ light/performance/test/perf_polymerase.py
@@ -1,4 +1,5 @@
-from os.path import dirname, join
+from os import mkdir
+from os.path import dirname, exists, isdir, join
 from unittest import TestCase
 import matplotlib.pyplot as plt
 from scipy import stats
@@ -20,16 +21,57 @@ _AFFINITY = affinityMatrix(_QUERIES, database=Database(testArgs.dbParams),
                            findParams=testArgs.findParams, returnDict=True)
 
 
-def plot(x, y, readId, scoreTypeX, scoreTypeY=None):
+def makeOutputDir(scoreTypeX, scoreTypeY):
+    """
+    Create an output directory if it doesn't already exist.
+
+    @param scoreTypeX: A C{str} X-axis title indicating the type of score.
+    @param scoreTypeY: A C{str} Y-axis title indicating the type of score.
+    @return: The C{str} path to the output directory.
+    """
+    FILESYSTEM_NAME = {
+        'Bit score': 'bit-score',
+        'Z score': 'z-score',
+        'Light matter score': 'lm-score',
+    }
+
+    assert scoreTypeX in FILESYSTEM_NAME and scoreTypeY in FILESYSTEM_NAME
+
+    def makeDir(path):
+        """
+        Check to see if a path exists and whether it's a directory. Create
+        it (as a dir) if it's non-existent.
+
+        @raise RuntimeError: if C{path} exists but is not a directory.
+        @param path: The C{str} path for the directory.
+        """
+        if exists(path):
+            if not isdir(path):
+                raise RuntimeError('Output path %r already exists but is not '
+                                   'a directory.' % path)
+        else:
+            mkdir(path)
+
+    outputDir = join(testArgs.outputDir, 'polymerase')
+
+    subDir = join(outputDir,
+                  '%s-vs-%s' % (FILESYSTEM_NAME[scoreTypeX],
+                                FILESYSTEM_NAME[scoreTypeY]))
+    makeDir(outputDir)
+    makeDir(subDir)
+
+    return subDir
+
+
+def plot(x, y, readId, scoreTypeX, scoreTypeY):
     """
     Make a scatterplot of the test results.
 
     @param x: a C{list} of C{float} x coordinates.
     @param y: a C{list} of C{float} y coordinates.
     @param readId: The C{str} id of the read whose values are being plotted.
-    @param scoreTypeX: A C{str} Y-axis title indicating the type of score.
-    @param scoreTypeY: A C{str} X-axis title indicating the type of score. To
-        be specified if a score other than the lm score is used.
+    @param scoreTypeX: A C{str} X-axis title indicating the type of score.
+    @param scoreTypeY: A C{str} Y-axis title indicating the type of score.
     """
     MAX_Z_SCORE = 60.0
 
@@ -51,14 +93,21 @@ def plot(x, y, readId, scoreTypeX, scoreTypeY=None):
     # Labels.
     ax.set_title('Read: %s, R^2: %.2f, SE: %.2f, slope: %.2f, p: %.2f' %
                  (readId, rValue, se, slope, pValue))
-    ax.set_ylabel(scoreTypeX)
-    if scoreTypeY:
-        ax.set_xlabel(scoreTypeY)
-    else:
-        ax.set_xlabel('Light matter score')
-        ax.set_xlim(0.0, 1.0)
+    ax.set_ylabel(scoreTypeY)
+    ax.set_xlabel(scoreTypeX)
 
+    # Light matter scores are always 0.0 to 1.0 so we can explicitly set
+    # axis limits for these.
+    if scoreTypeX == 'Light matter score':
+        ax.set_xlim(0.0, 1.0)
+    if scoreTypeY == 'Light matter score':
+        ax.set_ylim(0.0, 1.0)
+
+    # Z scores are always 0.0 to MAX_Z_SCORE so we can explicitly set
+    # axis limits for these.
     if scoreTypeX == 'Z score':
+        ax.set_xlim(0.0, MAX_Z_SCORE)
+    if scoreTypeY == 'Z score':
         ax.set_ylim(0.0, MAX_Z_SCORE)
 
     # Axes.
@@ -67,13 +116,8 @@ def plot(x, y, readId, scoreTypeX, scoreTypeY=None):
     ax.spines['bottom'].set_linewidth(0.5)
     ax.spines['left'].set_linewidth(0.5)
 
-    if scoreTypeY:
-        fig.savefig(join(testArgs.outputDir, '%s-%s-%s.png' % (readId,
-                                                               scoreTypeX,
-                                                               scoreTypeY)))
-    else:
-        fig.savefig(join(testArgs.outputDir, '%s-%s.png' % (readId,
-                                                            scoreTypeX)))
+    fig.savefig(join(makeOutputDir(scoreTypeX, scoreTypeY),
+                     '%s.png' % readId))
     plt.close()
 
 
@@ -90,7 +134,7 @@ class TestZScoreCorrelation(TestCase):
                 if queryId != subjectId:
                     lmScores.append(_AFFINITY[queryId][subjectId])
                     zScores.append(Z_SCORES[queryId][subjectId])
-            plot(lmScores, zScores, queryId, 'Z score')
+            plot(lmScores, zScores, queryId, 'Light matter score', 'Z score')
 
 
 class TestBitScoreCorrelation(TestCase):
@@ -106,7 +150,7 @@ class TestBitScoreCorrelation(TestCase):
                 if queryId != subjectId:
                     lmScores.append(_AFFINITY[queryId][subjectId])
                     zScores.append(BIT_SCORES[queryId][subjectId])
-            plot(lmScores, zScores, queryId, 'Bit score')
+            plot(lmScores, zScores, queryId, 'Light matter score', 'Bit score')
 
 
 class TestZScoreBitScoreCorrelation(TestCase):

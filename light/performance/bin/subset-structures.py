0210922c923c1e224f05058229305644b46c8fac
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2016-05-03 12:59:43 +0100

    Read write directly to stdin stdout

--- light/performance/bin/subset-structures.py
+++ light/performance/bin/subset-structures.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Read a fasta file of helices from stdin, and write all possible helix
+substrings to stdout.
+"""
+
 import sys
 
 from dark.fasta import FastaReads
 from dark.reads import AAReadWithX
 
 
-def makeSubsets(length, helix):
+def makeSubstring(length, helix):
     """
-    Take a string and make subsets of a specified length.
+    Take a string and make substrings of a specified length.
 
-    @param length: A C{int} of length of the subsets.
-    @param helix: A {dark.AAReadsWithX} instance of sequence that should be
-        subsetted.
+    @param length: An C{int} of length of the substrings.
+    @param helix: A {dark.AAReadsWithX} instance of sequence to make substrings
+        from.
     """
     assert(length < len(helix))
     numberOfHelices = len(helix) - length
@@ -19,39 +26,19 @@ def makeSubsets(length, helix):
         yield AAReadWithX(newHelixId, helix.sequence[i:length + i])
 
 
-def makeAllSubsets(helixFile, outFile):
-    """
-    Take a fasta file of helices, and write out a fasta file with all possible
-    helix subsets.
+allHelices = [helix for helix in FastaReads(sys.stdin,
+                                            readClass=AAReadWithX,
+                                            checkAlphabet=0)]
 
-    @param helixFile: A C{str} filename of helices to be subsetted.
-    @param outFile: A C{str} filename that the subsetted helices should be
-        written to.
-    """
-    allHelices = [helix for helix in FastaReads(helixFile,
-                                                readClass=AAReadWithX,
-                                                checkAlphabet=0)]
+allLengths = [len(helix) for helix in allHelices]
+minLength = min(allLengths)
+maxLength = max(allLengths)
 
-    allLengths = [len(helix) for helix in allHelices]
-    minLength = min(allLengths)
-    maxLength = max(allLengths)
-
-    with open(outFile, 'w') as ofp:
-        for length in range(minLength, maxLength + 1):
-            for helix in allHelices:
-                if length == len(helix):
-                    newHelix = AAReadWithX(helix.id, helix.sequence)
-                    print(newHelix.toString(format_='fasta'), file=ofp)
-                elif length < len(helix):
-                    for newHelix in makeSubsets(length, helix):
-                        print(newHelix.toString(format_='fasta'), file=ofp)
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        from os.path import basename
-        print('Usage: %s helixFile, outFile' % basename(sys.argv[0]),
-              file=sys.stderr)
-    else:
-        helixFile, outFile = sys.argv[1:]
-        makeAllSubsets(helixFile, outFile)
+for length in range(minLength, maxLength + 1):
+    for helix in allHelices:
+        if length == len(helix):
+            newHelix = AAReadWithX(helix.id, helix.sequence)
+            print(newHelix.toString(format_='fasta'), file=sys.stdout)
+        elif length < len(helix):
+            for newHelix in makeSubstring(length, helix):
+                print(newHelix.toString(format_='fasta'), file=sys.stdout)

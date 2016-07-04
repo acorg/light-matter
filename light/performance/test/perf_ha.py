ab67fbd4fbcddeef4aeb15beb9745f423cfddf51
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2016-07-04 15:29:55 +0100

    Corrections

--- light/performance/test/perf_ha.py
+++ light/performance/test/perf_ha.py
@@ -153,7 +153,7 @@ class TestBitScoreZScoreLightMatterScore3D(TestCase):
                    'Bit score', 'Z score', 'Light matter score',
                    dirName, testArgs.interactive)
 
-    def testPlots2HA3DPlotly(self):
+    def testPlotsHA3DPlotly(self):
         """
         Make a 3D plot of BLAST bit scores, Z scores, and light matter scores.
         """

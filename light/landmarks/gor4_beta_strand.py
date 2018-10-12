bf5a35093ed4abef6730c5d5fe707ebb0b930c8f
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2018-10-12 14:33:41 +0200

    Added light.gor4 script and changed gor4 landmark finders accordingly.

--- light/landmarks/gor4_beta_strand.py
+++ light/landmarks/gor4_beta_strand.py
@@ -2,7 +2,7 @@ from light.distance import scaleLog
 from light.features import Landmark
 from light.finder import Finder
 
-from gor4 import GOR4
+from light.gor4 import predictions
 
 
 class GOR4BetaStrand(Finder):
@@ -25,11 +25,10 @@ class GOR4BetaStrand(Finder):
         @param read: An instance of C{dark.reads.AARead}.
         @return: A generator that yields C{Landmark} instances.
         """
-        gor4 = GOR4()
-        predictions = gor4.predict(read.sequence)
+        preds = predictions(read.sequence)
         featureLengthBase = self._dbParams.featureLengthBase
         length = 0
-        for offset, prediction in enumerate(predictions['predictions']):
+        for offset, prediction in enumerate(preds):
             if prediction == 'E':
                 if length:
                     # We're already in a string of E's. Keep counting.
